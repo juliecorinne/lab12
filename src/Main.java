ca4d3b00588895124e5e08369631da7fe3622d7e
Julie Schneider <julieschneider@Julies-MacBook-Pro.local>
2017-02-10 16:41:48 -0500

    lab12

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
         Patrick patrick = new Patrick();
         User user = new User();
 
+
         rules();
 
         System.out.println("Who would you like to play against? Type 1 for Spongebob or 2 for Patrick.");
@@ -19,16 +20,47 @@ public class Main {
 
         if (team == 1) {
 
-            user.pick();
-            spongebob.pick();
+            String userPick = user.pick();
+            String team1 = spongebob.pick();
+
+            System.out.println("You picked " + userPick);
+            System.out.println("Spongebob picked " + team1);
+            System.out.println();
+
+            if (Objects.equals(userPick, team1)) {
+                System.out.println("It's a tie!");
+            } else if (Objects.equals(userPick, "R") && Objects.equals(team1, "S")) {
+                System.out.println("You win!");
+            } else if (Objects.equals(userPick, "S") && Objects.equals(team1, "R")) {
+                System.out.println("Spongebob wins!");
+            } else if (Objects.equals(userPick, "P") && Objects.equals(team1, "S")) {
+                System.out.println("Spongebob wins!");
+            } else if (Objects.equals(userPick, "S") && Objects.equals(team1, "P")) {
+                System.out.println("You win!");
+            } else if (Objects.equals(userPick, "P") && Objects.equals(team1, "R")) {
+                System.out.println("You win!");
+            } else if (Objects.equals(userPick, "R") && Objects.equals(team1, "P")) {
+                System.out.println("Spongebob wins!");
+            }
 
         } else {
 
-            user.pick();
-            patrick.pick();
+            String userPick = user.pick();
+            String team2 = patrick.pick();
+
+            System.out.println("You picked " + userPick);
+            System.out.println("Spongebob picked " + team2);
+            System.out.println();
+
+            if (Objects.equals(userPick, team2)) {
+                System.out.println("It's a tie!");
+            } else if (Objects.equals(userPick, "S") && Objects.equals(team2, "R")) {
+                System.out.println("Patrick wins!");
+            } else if (Objects.equals(userPick, "P") && Objects.equals(team2, "R")) {
+                System.out.println("You win!");
+            }
 
         }
-
     }
 
     public static void rules() {
